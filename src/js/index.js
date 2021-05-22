e09a84cddcc1bd9405875773357730426c0f9366
YuriFA <yurik45gb@gmail.com>
2021-05-22 13:56:30 +0300

    add desktop and mobile styles and html

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
-DOMReady(() => {});
+import Accordion from 'accordion-js';
+
+import { DOMReady } from './utils/DOMReady';
+
+DOMReady(() => {
+  const faqQuestions = Array.from(
+    document.querySelectorAll('.faq-questions'),
+  );
+  if (faqQuestions.length > 0) {
+    new Accordion(faqQuestions, {
+      duration: 300,
+      elementClass: 'faq-questions__item',
+      triggerClass: 'faq-questions__title',
+      panelClass: 'faq-questions__panel',
+    });
+  }
+});
