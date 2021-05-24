73f8db3a578b6d2ab2b40f8b465f3e6251e6bf32
YuriFA <yurik45gb@gmail.com>
2021-05-24 14:16:41 +0300

    fix html

--- src/js/index.js
+++ src/js/index.js
@@ -3,15 +3,15 @@ import Accordion from 'accordion-js';
 import { DOMReady } from './utils/DOMReady';
 
 DOMReady(() => {
-  const faqQuestions = Array.from(
-    document.querySelectorAll('.faq-questions'),
-  );
+  const faqQuestions = Array.from(document.querySelectorAll('.faq-questions'));
   if (faqQuestions.length > 0) {
     new Accordion(faqQuestions, {
-      duration: 300,
+      duration: 400,
       elementClass: 'faq-questions__item',
       triggerClass: 'faq-questions__title',
       panelClass: 'faq-questions__panel',
+      openOnInit: [0],
+      showMultiple: true,
     });
   }
 });
